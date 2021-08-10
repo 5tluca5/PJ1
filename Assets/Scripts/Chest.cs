24017eccfa1c55d4bb4e9eb95568938804182ce7
Lucas Ng <l34229776@gmail.com>
2021-08-10 18:10:03 +0800

    2nd update. #Youtube progress bar: 2:07:00.

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    public Sprite emptyChest;
+    public int pesosAmount = 5;
+
+    protected override void OnCollect()
+    {
+        if (collected) return;
+
+        base.OnCollect();
+
+        GetComponent<SpriteRenderer>().sprite = emptyChest;
+
+        Debug.Log("Chest collected.");
+        Debug.Log("Granted " + pesosAmount + " pesos.");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     private Vector3 delta;
     private BoxCollider2D boxCollider;
     private RaycastHit2D hit;
+    private Collider2D[] hits = new Collider2D[10];
 
     private void Start()
     {
@@ -20,8 +21,11 @@ public class Player : MonoBehaviour
 
         delta = new Vector3(x, y, 0);
 
+        bool canMoveHorizontally = true;
+        bool canMoveVertically = true;
+
         // Change moving direction
-        if(delta.x > 0)
+        if (delta.x > 0)
         {
             transform.localScale = Vector3.one;
         }
@@ -30,10 +34,23 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        // Check if there're any blocking object [left / right]
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(delta.x, 0), Mathf.Abs(delta.x * Time.deltaTime), LayerMask.GetMask("Blocking", "Actor"));
+        // Check what player are colliding
+        boxCollider.OverlapCollider(new ContactFilter2D(), hits);
 
-        if(hit.collider == null)
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i] == null) continue;
+
+            // do sth
+            // ...
+
+            hits[i] = null;
+        }
+
+        // Check if there're any blocking object [left / right]
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(delta.x, 0), Mathf.Abs(delta.x * Time.deltaTime), LayerMask.GetMask("Blocking", "Actor", "Stairs"));
+
+        if(hit.collider == null && canMoveHorizontally)
         {
             // Can move!
             transform.Translate(new Vector3(delta.x * Time.deltaTime, 0, 0));
@@ -42,7 +59,7 @@ public class Player : MonoBehaviour
         // Check if there're any blocking object [top / bottom]
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, delta.y), Mathf.Abs(delta.y * Time.deltaTime), LayerMask.GetMask("Blocking", "Actor"));
 
-        if (hit.collider == null)
+        if (hit.collider == null && canMoveVertically)
         {
             // Can move!
             transform.Translate(new Vector3(0, delta.y * Time.deltaTime, 0));
