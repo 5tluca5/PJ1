e8aea64b312ee4d970c47aa34fbcf5dcec6fd3b1
Lucas Ng <l34229776@gmail.com>
2021-08-24 18:16:12 +0800

    miss uploaded file.

---
+++ Assets/Scripts/EnemyHitbox.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitbox : Collidable
+{
+    // Damage
+    public int damage;
+    public float pushForce;
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if(coll.tag == "Flighter" && coll.name == "Player")
+        {
+            // Create a damage object, then send it to the player
+            Damage dmg = new Damage
+            {
+                damageAmount = damage,
+                pushForce = this.pushForce,
+                origin = this.transform.position
+            };
+
+            coll.SendMessage("ReceiveDamage", dmg);
+        }
+    }
+}
